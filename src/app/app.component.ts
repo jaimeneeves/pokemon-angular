cfb46bd0bc53b824d6823f2d57b9e3c739462fec
Jaime Neves <jaimeneves@MacBook-Air-de-Jaime.local>
2021-02-10 15:58:15 -0300

    Created service with findAll and findById method; Created empty pokemon component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PokemonService } from 'src/app/services/pokemon/pokemon.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pokemon-angular';
+
+  constructor(private pokemonService: PokemonService) {
+
+    pokemonService.findAll().subscribe({
+      next: (data: any) => {
+        console.log(data);
+      },
+      error: (e) => {
+
+      },
+      complete: () => {
+        console.info("Complete OK")
+      }
+    });
+  }
+
 }
