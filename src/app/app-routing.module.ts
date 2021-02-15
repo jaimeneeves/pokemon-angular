778da6734f3989763c550381f9cecf3dbf968466
Jaime Neves <jaimeneves@MacBook-Air-de-Jaime.local>
2021-02-15 08:54:36 -0300

    add useHash on router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,7 +24,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{
+    useHash: true,
+    enableTracing: false
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
